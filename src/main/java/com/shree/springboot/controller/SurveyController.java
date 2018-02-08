96cb669169a3b8f5fa3f07fc75896fd377862886
Sridharan Radhakrishnan <sridharan.radhakrishnan@avon.com>
2018-02-08 14:22:27 -0500

    logging changes

--- src/main/java/com/shree/springboot/controller/SurveyController.java
+++ src/main/java/com/shree/springboot/controller/SurveyController.java
@@ -3,6 +3,8 @@ package com.shree.springboot.controller;
 import java.net.URI;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,22 +14,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.shree.springboot.Application;
 import com.shree.springboot.model.Question;
 import com.shree.springboot.service.SurveyService;
 
 @RestController
 public class SurveyController {
-	
+	private static final Logger logger = LogManager.getLogger(SurveyController.class);
 	@Autowired
 	SurveyService service;
 	
 	@GetMapping(value="/surveys/{surveyId}/questions",produces="application/json")
 	public List<Question> retreiveQuestion(@PathVariable String surveyId){
+		logger.debug("Entering retreiveQuestion");
+		logger.debug("Exiting retreiveQuestion");
 		return service.retreiveQuestions(surveyId); 
+		
 	}
 	
 	@GetMapping(value="/surveys/{surveyId}/questions/{questionId}",produces="application/json")
 	public Question retreiveQuestion(@PathVariable String surveyId,@PathVariable String questionId){
+		
+		logger.debug("Entering retreiveQuestiona");
+		logger.debug("Exiting retreiveQuestiona");
 		return service.retreiveQuestion(surveyId, questionId);
 	}
 
