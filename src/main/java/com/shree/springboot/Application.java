65e7b4ae2fb0da68d8caace2407f6d73139f9930
Sridharan Radhakrishnan <sridharan.radhakrishnan@avon.com>
2018-02-08 14:24:25 -0500

    dlogging

--- src/main/java/com/shree/springboot/Application.java
+++ src/main/java/com/shree/springboot/Application.java
@@ -1,5 +1,7 @@
 package com.shree.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,10 +10,18 @@ import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
-	
+	private static final Logger logger = LogManager.getLogger(Application.class);
 	public static void main(String[] args) {
+		
+		
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-	}
+		 logger.debug("Debugging log");
+	        logger.info("Info log");
+	        logger.warn("Hey, This is a warning!");
+	        logger.error("Oops! We have an Error. OK");
+	        logger.fatal("Damn! Fatal error. Please fix me.");
+	    }
+	
 
 	@Profile("prod")
 	@Bean
